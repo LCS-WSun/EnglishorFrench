fc793ff3d62268413b89908ad5fa1c46ceae538c
Wesley Sun <wsun@lcs.on.ca>
2019-11-26 10:44:34 -0500

    Initial Commit

---
+++ EnglishorFrench/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EnglishorFrench
+//
+//  Created by Sun, Wesley on 2019-11-26.
+//  Copyright © 2019 Wesley Sun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
