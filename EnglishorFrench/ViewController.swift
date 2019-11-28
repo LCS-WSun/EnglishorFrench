989c063e1cb25c5a0ca302d4242ad868b8ddcc12
Wesley Sun <wsun@lcs.on.ca>
2019-11-27 19:40:11 -0500

    main story board done

--- EnglishorFrench/ViewController.swift
+++ EnglishorFrench/ViewController.swift
@@ -10,11 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var analyze: UIView!
+    
+    @IBOutlet weak var textview: UITextView!
+
+    @IBOutlet weak var label: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func AnalyzeText(_ sender: Any) {
+        
+    guard let input = textview, input.count < 0 else {
+            
+    outputAnalysis.text = "Please enter text to analyze." 
+        
+        }
+        
+    
+        
+        
+        
+        
+        
+            
+            
+            
+            
+            
+            
+            
+            
+            
+        }
+        
+        
+    }
+    
+    
+    
 }
 
